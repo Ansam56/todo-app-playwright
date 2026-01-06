760e4f0bd9bb6a64e3e91a51d9e4afec8f515dbf
ansam56 <ansamjanajreh.com>
2026-01-06 08:51:03 +0200

    add faker plugin

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,11 +1,13 @@
+import { faker } from "@faker-js/faker";
 import { test, expect } from "@playwright/test";
 
 test("should be able to add a todo", async ({ page }) => {
   await page.goto("/signup");
-  await page.locator('[data-testid="first-name"]').fill("Ansam");
-  await page.locator('[data-testid="last-name"]').fill("Janajreh");
-  // random email => later
-  await page.locator('[data-testid="email"]').fill("ansam1234@example.com");
+  await page
+    .locator('[data-testid="first-name"]')
+    .fill(faker.person.firstName());
+  await page.locator('[data-testid="last-name"]').fill(faker.person.lastName());
+  await page.locator('[data-testid="email"]').fill(faker.internet.email());
   await page.locator('[data-testid="password"]').fill("Test123*");
   await page.locator('[data-testid="confirm-password"]').fill("Test123*");
   await page.locator('[data-testid="submit"]').click();
@@ -25,16 +27,14 @@ test("should be able to add a todo", async ({ page }) => {
 
 test("should be able to delete a todo", async ({ page }) => {
   await page.goto("/signup");
-  await page.locator('[data-testid="first-name"]').fill("Ansam");
-  await page.locator('[data-testid="last-name"]').fill("Janajreh");
-  // random email => later
-  await page.locator('[data-testid="email"]').fill("ansam1234@example.com");
+  await page
+    .locator('[data-testid="first-name"]')
+    .fill(faker.person.firstName());
+  await page.locator('[data-testid="last-name"]').fill(faker.person.lastName());
+  await page.locator('[data-testid="email"]').fill(faker.internet.email());
   await page.locator('[data-testid="password"]').fill("Test123*");
   await page.locator('[data-testid="confirm-password"]').fill("Test123*");
   await page.locator('[data-testid="submit"]').click();
-
-  //await page.locator('[data-testid="add"]').click();
-  //await page.waitForTimeout(5000);
   await page.click('[data-testid="add"]');
   await page.locator('[data-testid="new-todo"]').fill("Learn Playwright");
   await page.locator('[data-testid="submit-newTask"]').click();
--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,11 +1,13 @@
 import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
 
 test("should be able to register to the doto website", async ({ page }) => {
   await page.goto("/signup");
-  await page.locator('[data-testid="first-name"]').fill("Ansam");
-  await page.locator('[data-testid="last-name"]').fill("Janajreh");
-  // random email => later
-  await page.locator('[data-testid="email"]').fill("ansam1@example.com");
+  await page
+    .locator('[data-testid="first-name"]')
+    .fill(faker.person.firstName());
+  await page.locator('[data-testid="last-name"]').fill(faker.person.lastName());
+  await page.locator('[data-testid="email"]').fill(faker.internet.email());
   await page.locator('[data-testid="password"]').fill("Test123*");
   await page.locator('[data-testid="confirm-password"]').fill("Test123*");
   await page.locator('[data-testid="submit"]').click();
